e9382a9b44c0e4f94ac35bce7c1ed415d2e355ea
Prakash <prakashm88@yahoo.co.in>
2018-05-05 18:33:54 +0000

    fixing folders

--- SkyWebPortal/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-select 1 from dual;
-INSERT INTO catalogue (category, product, locid) VALUES ( 'Sports', 'Arsenal TV', 'LONDON');
-INSERT INTO catalogue (category, product, locid) VALUES ( 'Sports', 'Chelsea TV', 'LONDON');
-INSERT INTO catalogue (category, product, locid) VALUES ( 'Sports', 'Liverpool TV', 'LIVERPOOL');
-INSERT INTO catalogue (category, product, locid) VALUES ( 'News', 'Sky News', '');
-INSERT INTO catalogue (category, product, locid) VALUES ( 'News', 'Sky Sports News', '');
-
-INSERT INTO customer (customerId,customerName,locid) VALUES ( 001, 'AA', 'LIVERPOOL');
-INSERT INTO customer (customerId,customerName,locid) VALUES ( 002, 'BB', 'LONDON');
